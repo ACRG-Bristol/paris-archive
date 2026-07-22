c2d459fb0716b4ede17bb0c133a548696a9d4253
joe-pitt <joseph.pitt@bristol.ac.uk>
2026-07-22 13:36:00 +0000

    reinstate pre-processing and post-processing for all sites

--- paris_archive/run.py
+++ paris_archive/run.py
@@ -16,21 +16,21 @@ def preprocess():
     paths = Paths("paris", site="zsf")
     zsf_ms_folder = data_file_path("", "paris", sub_path=getattr(paths, "GCMS-Medusa-flask_path"))
 
-    # files = glob(str(zsf_ms_folder) + "/*_air.nc")
-    # for f in files:
-    #     x = xr.open_dataset(f)
-    #     # Subtract 92 min offset from Cedric Couret email to Joe Pitt 2025-06-07
-    #     # 60 mins to put timestamp on UTC
-    #     # 32 mins to get from the chromatogram time to the start of sampling
-    #     x["sample_time"] = x["time"] - 92*60
-    #     x_std = xr.open_dataset(f.split("_")[-2] + "_std.nc")
-    #     spec_name = list(x.keys())[0].split("_")[0]
-    #     rep = x_std[spec_name + "_C"].sel(time=slice(1721865600,4000000000)).std()  # average for latest std
-    #     x[spec_name + "_std_stdev"] = x[spec_name + "_C"]
-    #     x[spec_name + "_std_stdev"].values = np.repeat(float(rep), x.sizes["time"])
-    #     x.to_netcdf(f + "_temp")
-    #     os.system("rm -f " + f)
-    #     os.system("mv " + f + "_temp " + f)
+    files = glob(str(zsf_ms_folder) + "/*_air.nc")
+    for f in files:
+        x = xr.open_dataset(f)
+        # Subtract 92 min offset from Cedric Couret email to Joe Pitt 2025-06-07
+        # 60 mins to put timestamp on UTC
+        # 32 mins to get from the chromatogram time to the start of sampling
+        x["sample_time"] = x["time"] - 92*60
+        x_std = xr.open_dataset(f.split("_")[-2] + "_std.nc")
+        spec_name = list(x.keys())[0].split("_")[0]
+        rep = x_std[spec_name + "_C"].sel(time=slice(1721865600,4000000000)).std()  # average for latest std
+        x[spec_name + "_std_stdev"] = x[spec_name + "_C"]
+        x[spec_name + "_std_stdev"].values = np.repeat(float(rep), x.sizes["time"])
+        x.to_netcdf(f + "_temp")
+        os.system("rm -f " + f)
+        os.system("mv " + f + "_temp " + f)
 
     # Now move ecd sf6 into the ms directory
     os.chdir(zsf_ms_folder)
@@ -71,25 +71,25 @@ def preprocess():
         os.system("rm -f " + f)
         os.system("mv " + f + "_temp " + f)
 
-    # # Now CMN
-    # paths = Paths("paris", site="cmn")
-    # cmn_folder = data_file_path("", "paris", sub_path=getattr(paths, "GCMS-Medusa-flask_path"))
+    # Now CMN
+    paths = Paths("paris", site="cmn")
+    cmn_folder = data_file_path("", "paris", sub_path=getattr(paths, "GCMS-Medusa-flask_path"))
 
-    # files = glob(str(cmn_folder) + "/*_air.nc")
-    # for f in files:
-    #     x = xr.open_dataset(f)
-    #     x["sample_time"] = x["time"]
-    #     x.to_netcdf(f + "_temp")
-    #     os.system("rm -f " + f)
-    #     os.system("mv " + f + "_temp " + f)
+    files = glob(str(cmn_folder) + "/*_air.nc")
+    for f in files:
+        x = xr.open_dataset(f)
+        x["sample_time"] = x["time"]
+        x.to_netcdf(f + "_temp")
+        os.system("rm -f " + f)
+        os.system("mv " + f + "_temp " + f)
 
-    # # Now HUN
-    # paths = Paths("paris", site="hun")
-    # hun_folder = data_file_path("", "paris", sub_path=getattr(paths, "GCMS-Medusa-flask_path"))
-    # os.chdir(hun_folder)
-    # os.system("cp ../taunus-ecd_HUN_flask/sf6_air.nc ./sf6_air.nc")
+    # Now HUN
+    paths = Paths("paris", site="hun")
+    hun_folder = data_file_path("", "paris", sub_path=getattr(paths, "GCMS-Medusa-flask_path"))
+    os.chdir(hun_folder)
+    os.system("cp ../taunus-ecd_HUN_flask/sf6_air.nc ./sf6_air.nc")
 
-    # os.chdir(current_dir)
+    os.chdir(current_dir)
 
 def postprocess():
     """Postprocess data files before running the main script 
@@ -123,12 +123,12 @@ def postprocess():
             os.system("rm -f " + f)
             os.system("mv " + f + "_temp " + f)
 
-    # # Also change TOB SF6
-    # files = glob("paris-archive/sf6/paris_hun*.nc")
-    # x = xr.open_dataset(files[0])
-    # x["instrument_type"].attrs["comment"] = "UNDEFINED=-1, GCECD-flask=0"
-    # x.attrs["instrument_type"] = "GCECD-flask"
-    # x.attrs["comment"] = x.attrs["comment"].replace("GCMS Medusa flask", "GCECD flask")
+    # Also change TOB SF6
+    files = glob("paris-archive/sf6/paris_hun*.nc")
+    x = xr.open_dataset(files[0])
+    x["instrument_type"].attrs["comment"] = "UNDEFINED=-1, GCECD-flask=0"
+    x.attrs["instrument_type"] = "GCECD-flask"
+    x.attrs["comment"] = x.attrs["comment"].replace("GCMS Medusa flask", "GCECD flask")
 
     # Now rezip
     os.system("rm paris-archive.zip")
@@ -142,11 +142,11 @@ def postprocess():
     os.chdir(zsf_ms_folder)
     os.system("rm sf6_air.nc")
 
-    # os.chdir(current_dir)
-    # paths = Paths("paris", site="hun")
-    # hun_folder = data_file_path("", "paris", sub_path=getattr(paths, "GCMS-Medusa-flask_path"))
-    # os.chdir(hun_folder)
-    # os.system("rm sf6_air.nc")
+    os.chdir(current_dir)
+    paths = Paths("paris", site="hun")
+    hun_folder = data_file_path("", "paris", sub_path=getattr(paths, "GCMS-Medusa-flask_path"))
+    os.chdir(hun_folder)
+    os.system("rm sf6_air.nc")
     
 
 if __name__ == "__main__":
@@ -154,7 +154,6 @@ if __name__ == "__main__":
     preprocess()
 
     run_all("paris",
-            sites=["BIR","ZSF"],
             combined=False,
             baseline=False,
             monthly=False,
